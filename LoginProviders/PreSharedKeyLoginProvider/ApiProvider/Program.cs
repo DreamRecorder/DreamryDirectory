9201eb1b1c530e34095fe61a5868ea0d5492b8ee
Wencey Wang <WenceyWang@dreamry.org>
2020-12-19 08:05:25 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/DreamryDirectory;C599

--- LoginProviders/PreSharedKeyLoginProvider/ApiProvider/Program.cs
+++ LoginProviders/PreSharedKeyLoginProvider/ApiProvider/Program.cs
@@ -3,23 +3,68 @@ using System.Collections ;
 using System.Collections.Generic ;
 using System.Linq ;
 
+using DreamRecorder . ToolBox . CommandLine ;
+using DreamRecorder . ToolBox . General ;
+
 using Microsoft.AspNetCore.Hosting ;
 using Microsoft.Extensions.Hosting ;
+using Microsoft . Extensions . Logging ;
 
 namespace DreamRecorder . Directory . LoginProviders . PreSharedKeyLoginProvider . ApiService
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+		: ProgramBase<Program, Program.ProgramExitCode, Program.ProgramSetting, Program.ProgramSettingCatalog>
+	{
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		public enum ProgramSettingCatalog
+		{
+
+		}
+
+		public override bool CheckLicense => false;
+
+		public override bool WriteLicenseFile => false;
+
+		public override bool WaitForExit => false;
+
+		public override bool LoadPlugin => false;
+
+		public override string License => string.Empty;
+
+		public override bool CanExit => true;
+
+		public override bool HandleInput => true;
+
+		public override bool LoadSetting => false;
+
+		public override bool AutoSaveSetting => false;
+
+		public static void Main(string[] args) { new Program().RunMain(args); }
+
+		public static IHostBuilder CreateHostBuilder(string[] args)
+			=> Host.CreateDefaultBuilder(args).
+					UseServiceProviderFactory(new StaticServiceProviderFactory()).
+					ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+
+		public override void Start(string[] args) { CreateHostBuilder(args).Build().Run(); }
+
+		public override void ConfigureLogger(ILoggingBuilder builder) { builder.AddDebug(); }
+
+		public override void ShowLogo() { }
+
+		public override void ShowCopyright() { }
+
+		public override void OnExit(ProgramExitCode code) { }
+
+		public class ProgramSetting : SettingBase<ProgramSetting, ProgramSettingCatalog>
+		{
+
+		}
+
+		public class ProgramExitCode : ProgramExitCode<ProgramExitCode>
+		{
+
+		}
+
+	}
 }
