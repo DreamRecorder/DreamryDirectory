cff4a00d3d9cf063795f9f824589945696e57ee6
Wencey Wang <WenceyWang@dreamry.org>
2022-02-09 14:37:27 +0000

    git-tfs-id: [https://vsts.dreamry.org/DefaultCollection/]$/DreamryDirectory;C771

--- Logic/Logic/ILoginService.cs
+++ Logic/Logic/ILoginService.cs
@@ -1,37 +1,66 @@
-﻿using System ;
-using System . Collections ;
-using System . Collections . Generic ;
-using System . Linq ;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
-using DreamRecorder . Directory . Logic . Tokens ;
+using DreamRecorder.Directory.Logic.Tokens;
+using DreamRecorder.ToolBox.General;
 
-namespace DreamRecorder . Directory . Logic
+namespace DreamRecorder.Directory.Logic
 {
 
-	public interface ILoginService : IService
-	{
+    public interface ILoginService : IService
+    {
 
-		LoginToken Login ( object credential ) ;
+        LoginToken Login(object credential);
 
-		void CheckToken ( AccessToken token , LoginToken tokenToCheck ) ;
+        void CheckToken(AccessToken token, LoginToken tokenToCheck);
 
-		void DisposeToken ( LoginToken token ) ;
+        void DisposeToken(LoginToken token);
 
-	}
+    }
 
-	public abstract class ServiceBase : IService
-	{
+    public abstract class ServiceBase : IService, IStartStop
+    {
 
-		public DateTimeOffset ? StartupTime { get ; private set ; }
+        public DateTimeOffset? StartupTime { get; private set; }
 
-		public DateTimeOffset GetStartupTime ( ) => StartupTime ?? DateTimeOffset . Now ;
+        public DateTimeOffset GetStartupTime() => StartupTime ?? DateTimeOffset.Now;
 
-		public DateTimeOffset GetTime ( ) => DateTime . Now ;
+        public DateTimeOffset GetTime() => DateTime.Now;
 
-		public virtual Version GetVersion ( ) => GetType ( ) . Assembly . GetName ( ) . Version ;
+        public virtual Version GetVersion() => GetType().Assembly.GetName().Version;
 
-		public virtual void Start ( ) { StartupTime ??= DateTimeOffset . Now ; }
+        public virtual void Start()
+        {
+            lock (this)
+            {
+                if (StartupTime == null)
+                {
+                    StartupTime ??= DateTimeOffset.Now;
+                }
+            }
+        }
 
-	}
+        public void Stop()
+        {
+            lock (this)
+            {
+                StartupTime = null;
+            }
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                lock (this)
+                {
+                    return StartupTime != null;
+                }
+            }
+        }
+
+    }
 
 }
